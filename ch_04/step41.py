91b01ca7f86a0e6c01beaafe71f88d00c3a66202
zishi <zishi@qq.com>
2023-05-15 08:49:26 +0800

    矩阵的乘积和反向传播

--- ch_04/step41.py
+++ ch_04/step41.py
@@ -11,3 +11,13 @@ x2 = np.array([[5, 6], [7, 8]])
 
 y = np.dot(x1, x2)
 print(y)
+
+
+# 测试 MatMul
+x3 = Variable(np.array([[1, 2], [3, 4]]))
+x4 = Variable(np.array([[5, 6], [7, 8]]))
+y = F.matmul(x3, x4)
+y.backward()
+
+print(x3.grad)
+print(x4.grad)
