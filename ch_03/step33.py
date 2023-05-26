75715fb1462865532cd29461f769275fecd76594
guo20082200 <zishi@example.com>
2023-05-26 15:54:16 +0800

    计算图的可视化

--- ch_03/step33.py
+++ ch_03/step33.py
@@ -1,41 +1,26 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
-from scipy.optimize import rosen
-import matplotlib.pyplot as plt
-
-from dezero.core import Variable
-
+from dezero import Variable
 
 def f(x):
     y = x ** 4 - 2 * x ** 2
     return y
 
-
 x = Variable(np.array(2.0))
-
-# y = f(x)
-# print(y)  # variable(8.0)
-#
-# y.backward(create_graph=True)
-# print(x.grad)  # variable(-32.0)
-#
-# gx = x.grad
-# x.clean_grad()
-# gx.backward()
-# print(x.grad)  # variable(-80.0)
-
-# 使用牛顿法进行优化
 iters = 10
+
 for i in range(iters):
-    # print(i, x)
+    print(i, x)
+
     y = f(x)
-    x.clean_grad()
+    x.cleargrad()
     y.backward(create_graph=True)
 
     gx = x.grad
-    x.clean_grad()
+    x.cleargrad()
     gx.backward()
     gx2 = x.grad
 
     x.data -= gx.data / gx2.data
-
-    print(i, x , x.data)
