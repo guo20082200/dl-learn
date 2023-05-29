94f9cbd288df916628c3269a1c3da8b9698f9c43
guo20082200 <zishi@example.com>
2023-05-29 13:18:49 +0800

    step27 泰勒级数

--- ch_03/step27.py
+++ ch_03/step27.py
@@ -1,10 +1,15 @@
 if '__file__' in globals():
     import os, sys
+
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 import math
 from dezero import Variable, Function
 from dezero.utils import plot_dot_graph
+
+"""
+    泰勒级数展开
+"""
 
 
 class Sin(Function):
@@ -30,7 +35,13 @@ print(y.data)
 print(x.grad)
 
 
-def my_sin(x, threshold=0.0001):
+def my_sin(x, threshold=1e-150):
+    """
+        使用泰勒级数来求sin(x)的值
+    :param x:
+    :param threshold: 最后一项的阈值
+    :return: 正弦值
+    """
     y = 0
     for i in range(100000):
         c = (-1) ** i / math.factorial(2 * i + 1)
