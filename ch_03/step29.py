c505316b299fd20a6b48c6aaf8180a526b37f084
guo20082200 <zishi@example.com>
2023-05-29 14:19:36 +0800

    step29 牛顿法

--- ch_03/step29.py
+++ ch_03/step29.py
@@ -1,13 +1,16 @@
 if '__file__' in globals():
     import os, sys
+
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 from dezero import Variable
 # import dezero's simple_core explicitly
 import dezero
+
 if not dezero.is_simple_core:
     from dezero.core_simple import Variable
     from dezero.core_simple import setup_variable
+
     setup_variable()
 
 
@@ -30,4 +33,20 @@ for i in range(iters):
     x.cleargrad()
     y.backward()
 
+    """
+        使用牛顿法来更新x的数据
+    """
     x.data -= x.grad / gx2(x.data)
+
+# 迭代了7次之后就达到最优解了
+# ---------------------------------------
+# 0 variable(2.0)
+# 1 variable(1.4545454545454546)
+# 2 variable(1.1510467893775467)
+# 3 variable(1.0253259289766978)
+# 4 variable(1.0009084519430513)
+# 5 variable(1.0000012353089454)
+# 6 variable(1.000000000002289)
+# 7 variable(1.0)
+# 8 variable(1.0)
+# 9 variable(1.0)
