91cd86d43d087cce66594d46d5e69f1abb209775
guo20082200 <zishi@example.com>
2023-05-29 19:15:04 +0800

    张量计算

--- ch_04/step37.py
+++ ch_04/step37.py
@@ -1,9 +1,14 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
+from dezero.core import Variable
+
 from scipy.optimize import rosen
 import matplotlib.pyplot as plt
 
-from dezero.core import Variable
 import dezero.functions as F
+
 
 x = Variable(np.array([[1, 2, 3], [4, 6, 7]]))
 c = Variable(np.array([[1, 2, 3], [4, 6, 7]]))
@@ -18,5 +23,7 @@ print(x + c)
 
 # 测试 reshape
 x = Variable(np.array([[1, 2, 3], [4, 6, 7]]))
+print(x)
 
 
+# 测试ndarray的广播功能
--- dezero/functions.py
+++ dezero/functions.py
@@ -87,27 +87,27 @@ def log(x):
 # =============================================================================
 # Tensor operations: reshape / transpose / get_item / expand_dims / flatten
 # =============================================================================
-#
-#
-# class Reshape(Function):
-#
-#     def __init__(self, shape):
-#         self.shape = shape
-#
-#     def forward(self, x):
-#         self.x_shape = x.shape
-#         y = x.reshape(self.shape)
-#         return y
-#
-#     def backward(self, gy):
-#         return reshape(gy, self.x_shape)
-#
-#
-# def reshape(x, shape):
-#     if x.shape == shape:
-#         return as_variable(x)
-#     return Reshape(shape)(x)
-#
+
+
+class Reshape(Function):
+
+    def __init__(self, shape):
+        self.shape = shape
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.reshape(self.shape)
+        return y
+
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
+
+
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
+
 #
 # class Transpose(Function):
 #
