01305b74aa15333731485d1e14e5403abc80c0b7
guo20082200 <zishi@example.com>
2023-06-01 16:09:49 +0800

    step43 神经网络

--- ch_04/step43.py
+++ ch_04/step43.py
@@ -1,26 +1,30 @@
 if '__file__' in globals():
     import os, sys
+
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import sys  # 导入sys模块
+if '__file__' in globals():
+    import os, sys
 
-sys.setrecursionlimit(10000)  # 将默认的递归深度修改为3000
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 import matplotlib.pyplot as plt
 from dezero import Variable
 import dezero.functions as F
 
-
 np.random.seed(0)
 x = np.random.rand(100, 1)
-
 y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
 
 I, H, O = 1, 10, 1
 W1 = Variable(0.01 * np.random.randn(I, H))
+print(W1.data.shape)  # (1, 10)
 b1 = Variable(np.zeros(H))
+print(b1.data.shape)  # (10,)
 W2 = Variable(0.01 * np.random.randn(H, O))
+print(W2.data.shape)  # (10, 1)
 b2 = Variable(np.zeros(O))
+print(b2.data.shape)  # (1,)
 
 
 def predict(x):
@@ -33,16 +37,14 @@ def predict(x):
 lr = 0.2
 iters = 10000
 
-print(type(y))
-
 for i in range(iters):
     y_pred = predict(x)
     loss = F.mean_squared_error(y, y_pred)
 
-    W1.clean_grad()
-    b1.clean_grad()
-    W2.clean_grad()
-    b2.clean_grad()
+    W1.cleargrad()
+    b1.cleargrad()
+    W2.cleargrad()
+    b2.cleargrad()
     loss.backward()
 
     W1.data -= lr * W1.grad.data
@@ -52,7 +54,6 @@ for i in range(iters):
     if i % 1000 == 0:
         print(loss)
 
-
 # Plot
 plt.scatter(x, y, s=10)
 plt.xlabel('x')
@@ -60,4 +61,4 @@ plt.ylabel('y')
 t = np.arange(0, 1, .01)[:, np.newaxis]
 y_pred = predict(t)
 plt.plot(t, y_pred.data, color='r')
-plt.show()
+plt.savefig("step43-nn.png")
--- dezero/functions.py
+++ dezero/functions.py
@@ -240,56 +240,56 @@ def mean_squared_error_simple(x0, x1):
     return sum(diff ** 2) / len(diff)
 
 
-# def linear_simple(x, W, b=None):
-#     t = matmul(x, W)
-#     if b is None:
-#         return t
-#     y = t + b
-#     t.data = None  # 删除t的数据
-#     return y
-#
-#
-# class Linear(Function):
-#     def forward(self, x, W, b):
-#         # y = x.dot(W)
-#         y = matmul(x, W)
-#         if b is not None:
-#             y += b
-#         return y
-#
-#     def backward(self, gy):
-#         x, W, b = self.inputs
-#         gb = None if b.data is None else sum_to(gy, b.shape)
-#         gx = matmul(gy, W.T)
-#         gW = matmul(x.T, gy)
-#         return gx, gW, gb
-#
-#
-# def linear(x, W, b=None):
-#     return Linear()(x, W, b)
-#
-#
-# # =============================================================================
-# # activation function: sigmoid / relu / softmax / log_softmax / leaky_relu
-# # =============================================================================
-# def sigmoid_simple(x):
-#     x = as_variable(x)
-#     y = 1 / (1 + exp(-x))
-#     return y
-#
-#
-# class Sigmoid(Function):
-#     def forward(self, x):
-#         # xp = cuda.get_array_module(x)
-#         y = 1 / (1 + exp(-x))
-#         # y = tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
-#         return y
-#
-#     def backward(self, gy):
-#         y = self.outputs[0]()
-#         gx = gy * y * (1 - y)
-#         return gx
-#
-#
-# def sigmoid(x):
-#     return Sigmoid()(x)
+def linear_simple(x, W, b=None):
+    t = matmul(x, W)
+    if b is None:
+        return t
+    y = t + b
+    t.data = None  # 删除t的数据
+    return y
+
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        # y = matmul(x, W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+# =============================================================================
+# activation function: sigmoid / relu / softmax / log_softmax / leaky_relu
+# =============================================================================
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
+
+
+class Sigmoid(Function):
+    def forward(self, x):
+        # xp = cuda.get_array_module(x)
+        y = 1 / (1 + np.exp(-x))
+        # y = tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
