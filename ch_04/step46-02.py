2cac61ee12ef15748fd58bff3f9994d28544ce99
guo20082200 <zishi@example.com>
2023-06-02 14:44:40 +0800

    optimizer

---
+++ ch_04/step46-02.py
@@ -0,0 +1,40 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from dezero import optimizers
+import dezero.functions as F
+from dezero.models import MLP
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+lr = 0.2
+max_iter = 10000
+hidden_size = 10
+
+model = MLP((hidden_size, 1))
+optimizer = optimizers.MomentumSGD(lr).setup(model)
+
+for i in range(max_iter):
+    y_pred = model(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    model.cleargrads()
+    loss.backward()
+
+    optimizer.update()
+    if i % 1000 == 0:
+        print(loss)
+
+# Plot
+plt.scatter(x, y, s=10)
+plt.xlabel('x')
+plt.ylabel('y')
+t = np.arange(0, 1, .01)[:, np.newaxis]
+y_pred = model(t)
+# print(y_pred)
+plt.plot(t, y_pred.data, color='r')
+plt.savefig("step46-02-optimizer.png")
